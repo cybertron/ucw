dbb51545a5bc7ab0acb9bc262c18d52a6da1cefc
Ben Nemec <bnemec@redhat.com>
2016-05-09 18:32:36 +0000

    Add local.sh for easier testing
    Makes it easier to start an instance of the application locally for
    testing purposes.

--- app.py
+++ app.py
@@ -2,12 +2,14 @@
 
 import os
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
+# When we're running locally we don't need to do this.
+if 'OPENSHIFT_PYTHON_DIR' in os.environ:
+    virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+    virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+    try:
+        execfile(virtualenv, dict(__file__=virtualenv))
+    except IOError:
+        pass
 
 # requires: pyramid, netaddr, jinja2
 import sys
---
+++ local.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Simple script for running the application locally
+
+export OPENSHIFT_PYTHON_IP=0.0.0.0
+export OPENSHIFT_PYTHON_PORT=8080
+./app.py
