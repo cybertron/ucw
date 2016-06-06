ee5d569006dfb47a9d5f518855fb3ef567531044
Ben Nemec <bnemec@redhat.com>
2016-06-06 21:31:41 +0000

    Use nested dicts for parameter descriptions
    Jinja understands dicts, so this is better than namespacing params
    by simply prepending the group name.

--- update-descriptions.py
+++ update-descriptions.py
@@ -6,8 +6,9 @@ from instack_undercloud import undercloud
 
 descriptions = {}
 for group, opts in undercloud.list_opts():
+    new_descriptions = {}
     for opt in opts:
-        key = '%s_%s' % (group or 'DEFAULT', opt.name)
-        descriptions[key] = opt.help
+        new_descriptions[opt.name] = opt.help
+    descriptions[group or 'DEFAULT'] = new_descriptions
 with open('opt-descriptions.json', 'w') as f:
     f.write(json.dumps(descriptions))
